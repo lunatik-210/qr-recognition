a9212a022bf4f7cb0d03609e228771c5c2cfc58e
Andrew Lapin <andrew.d.lapin@gmail.com>
2012-08-06 23:05:01 +0400

    Implemented contour validation algorithm

--- src/simplefilter.cpp
+++ src/simplefilter.cpp
@@ -12,28 +12,32 @@ IplImage* SimpleFilter::process( IplImage* frame )
     cvThreshold( gray, gray, 100, 255, CV_THRESH_BINARY );
 
     //создание хранилище памяти
-    CvMemStorage* storage=cvCreateMemStorage( 0 );
+    CvMemStorage* storage = cvCreateMemStorage( 0 );
 
     CvSeq* firstContour = NULL;
-    cvFindContours( gray, storage, &firstContour, sizeof( CvContour ), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint( 0, 0 ) );
+    cvFindContours( gray, storage, &firstContour, sizeof( CvContour ), CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cvPoint( 0, 0 ) );
 
     QVector<CvRect> rects;
 
-    for( CvSeq* seq0 = firstContour; seq0!=0; seq0 = seq0->h_next )
+    while( ( firstContour = nextSeq( firstContour ) ) != NULL )
     {
-        CvRect rect = cvBoundingRect( seq0 );
-        CvBox2D box = cvMinAreaRect2( seq0 );
-        CvSeq* c_temp=0;
+        if( !isValidContour( firstContour ) )
+        {
+            continue;
+        }
+
+        CvRect rect = cvBoundingRect( firstContour );
+        CvBox2D box = cvMinAreaRect2( firstContour );
         double countourArea = 0.0;
         double i1, i2 = 0.0;
-        
+
         if( box.size.height < 0.1 || box.size.width < 0.1 )
         {
             i1=0;
         }
         else
         {
-            countourArea = fabs( cvContourArea( seq0 ) );
+            countourArea = fabs( cvContourArea( firstContour ) );
             i1 = countourArea / (double)( box.size.width * box.size.height );
             i2 =( ( box.size.width < box.size.height ) ?
                (double) box.size.width / (double) box.size.height:
@@ -41,29 +45,60 @@ IplImage* SimpleFilter::process( IplImage* frame )
         }
         if( i1>=0.65 && i2>=0.65 && abs( box.size.width * box.size.height - countourArea ) < countourArea * 0.3 )
         {
-            c_temp=seq0;
             rects.push_back( rect );
         }
-        else
-        {
-            if ( c_temp == 0 )
-                firstContour=firstContour->h_next;
-            else
-                c_temp->h_next=c_temp->h_next->h_next;
-        }
     }
+
 
     for( int indx = 0; indx < rects.size(); ++indx )
     {
         CvPoint p1, p2;
-        p1.x=rects[indx].x;
-        p1.y=rects[indx].y;
-        p2.x=rects[indx].x+rects[indx].width;
-        p2.y=rects[indx].y+rects[indx].height;
+        p1.x = rects[indx].x;
+        p1.y = rects[indx].y;
+        p2.x = rects[indx].x+rects[indx].width;
+        p2.y = rects[indx].y+rects[indx].height;
         cvRectangle( frame, p1, p2, CV_RGB(30,216,30), 1, 8, 0 );
     }
 
     cvReleaseImage( &gray );
     cvReleaseMemStorage( &storage );
     return frame;
+}
+
+bool SimpleFilter::isValidContour( CvSeq* contour )
+{
+    CvSeq* seq = contour;
+    int c = 0;
+    while( ( seq = seq->v_next ) != NULL )
+    {
+        ++c;
+    }
+
+    if( c != 2 )
+    {
+        return false;
+    }
+    return true;
+}
+
+CvSeq* SimpleFilter::nextSeq( CvSeq* current )
+{
+    if( !current )
+    {
+        return NULL;
+    }
+
+    CvSeq* seq = NULL;
+
+    seq = current->h_next;
+
+    if( seq != 0 )
+        return seq;
+
+    seq = current->v_next;
+
+    if( seq != 0 )
+        return seq;
+
+    return NULL;
 }
--- src/simplefilter.h
+++ src/simplefilter.h
@@ -11,6 +11,11 @@ public:
 
     virtual IplImage* process( IplImage* frame );
 
+protected:
+    CvSeq* nextSeq( CvSeq* current );
+
+    bool isValidContour( CvSeq* conture );
+
 };
 
 #endif // SIMPLEFILTER_H
